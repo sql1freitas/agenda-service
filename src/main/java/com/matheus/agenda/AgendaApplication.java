050c23408f026a026245c7dccb80bebf1528c5b9
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-01-03 17:10:56 -0300

    Adicionado OpenAPI para utilização do Swagger3

--- src/main/java/com/matheus/agenda/AgendaApplication.java
+++ src/main/java/com/matheus/agenda/AgendaApplication.java
@@ -1,5 +1,5 @@
 package com.matheus.agenda;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.matheus.agenda")
 @Configuration
 @SpringBootApplication
+@OpenAPIDefinition
 public class AgendaApplication {
 
 	public static void main(String[] args) {
